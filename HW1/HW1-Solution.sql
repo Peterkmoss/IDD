c990cf3cadca93fbf73238c240609495fcd6fa95
Peterkmoss <peter@moss.dk>
2019-09-10 10:48:37 +0200

    HW1 optimized

--- HW1/HW1-Solution.sql
+++ HW1/HW1-Solution.sql
@@ -11,17 +11,21 @@ Sports(id, name, record)
 */
 
 -- 1
+/*
+\echo 'Ex. 1';
 select count(distinct R.peopleid)
 from results R
 where R.result is null;
 
 -- 2
+\echo 'Ex. 2';
 select P.id, P.name
 from people P
   left join results R on P.id = R.peopleid
 where R.competitionid is null;
 
 -- 3
+\echo 'Ex. 3';
 select distinct P.id, P.name
 from people P
   join results R on P.id = R.peopleid
@@ -31,6 +35,7 @@ where (S.name = 'High Jump' and S.record = R.result)
    or (extract(month from C.held) = 6 and extract(year from C.held) = 2002);
 
 -- 4
+\echo 'Ex. 4';
 select P.id, P.name
 from people P
   join results R on P.id = R.peopleid
@@ -44,22 +49,25 @@ group by P.id, P.name
 having count(R.sportid) = 1;
 
 -- 5
+\echo 'Ex. 5';
 select S.id, S.name, to_char(MAX(R.result), '90.99') as maxres
 from sports S
   join results R on S.id = R.sportid
-group by S.id, S.name
+group by S.id
 order by S.id;
 
 -- 6
+\echo 'Ex. 6';
 select P.id, P.name, count(R.result) as total
 from people P
   join results R on P.id = R.peopleid
   join sports S on R.sportid = S.id
 where R.result = S.record
-group by P.id, P.name
+group by P.id
   having count(distinct R.sportid) >= 2;
 
 -- 7
+\echo 'Ex. 7';
 select distinct P.id, P.name, P.height, R.result, S.name, case 
                                                             when R.result = S.record then 'Yes' 
                                                             else 'No' end as "record?"
@@ -71,6 +79,7 @@ from people P
         group by R.sportid) best on best.sportid = R.sportid and best.maxres = R.result;
 
 -- 8
+\echo 'Ex. 8';
 select count(*)
 from (
     select count(*)
@@ -82,25 +91,29 @@ from (
 ) tmp;
 
 -- 9
-
---insert into people values (6969, 'Peter Moss', 'M', 1.84); 
---insert into results values (6969, 50, 0, 2.11);
---insert into results values (6969, 50, 1, 6.78);
---insert into results values (6969, 50, 2, 13.15);
---insert into results values (6969, 50, 3, 16.66);
---insert into results values (6969, 50, 4, 5.52);
---insert into results values (6969, 50, 5, 60.46);
---insert into results values (6969, 50, 6, 50.41);
+\echo 'Ex. 9';
+insert into people values (6969, 'Peter Moss', 'M', 1.84); 
+insert into results values (6969, 50, 0, 2.11);
+insert into results values (6969, 50, 1, 6.78);
+insert into results values (6969, 50, 2, 13.15);
+insert into results values (6969, 50, 3, 16.66);
+insert into results values (6969, 50, 4, 5.52);
+insert into results values (6969, 50, 5, 60.46);
+insert into results values (6969, 50, 6, 50.41);
 
 select P.id, P.name
 from people P
   join results R on P.id = R.peopleid
   join sports S on R.sportid = S.id
 where R.result = S.record
-group by P.id, P.name
-  having count(distinct R.sportid) = (select count(id) from sports);
+group by P.id
+  having count(distinct R.sportid) = (select count(*) from sports);
+
+delete from results where peopleid = 6969;
+delete from people where id = 6969;
 
 -- 10
+\echo 'Ex. 10';
 select S.id, S.name, S.record, MIN(R.result)
 from sports S
   join results R on S.id = R.sportid
@@ -108,7 +121,7 @@ from sports S
 where R.result is not null
 group by S.id, S.name, S.record
   having count(distinct C.place) = (select count(distinct place) from competitions);
-
+*/
 -- Part 2
 /*
 
@@ -121,16 +134,18 @@ role(role)
 involved(personid, movieid, role)
 
 */
-/*
+
 -- 11
+\echo 'Ex. 11';
 select count(*)
 from person
 where height is null;
 
 -- 12
+\echo 'Ex. 12';
 select count(*)
 from (
-  select avg(p.height)
+  select i.movieid
   from involved i
     join person p on i.personid = p.id
   where p.height is not null
@@ -139,6 +154,7 @@ from (
 ) tmp;
 
 -- 13
+ \echo 'Ex. 13';
 select count(*)
 from (
   select count(*)
@@ -148,6 +164,7 @@ from (
 ) tmp;
 
 -- 14
+\echo 'Ex. 14';
 select count(distinct p.id)
 from person p
   join involved i on p.id = i.personid
@@ -159,16 +176,15 @@ from person p
 where i.role = 'actor';
 
 -- 15
-
+\echo 'Ex. 15';
 select count(*)
 from movie m
   left join involved i on m.id = i.movieid
 where i.movieid is null
   and m.year = 1999;
 
-
 -- 16
-
+\echo 'Ex. 16';
 select count(*)
 from (
 select i_actor.personid
@@ -181,9 +197,8 @@ group by i_actor.personid
 having count(*) >= 2
 ) tmp;
 
-
 -- 17
-
+\echo 'Ex. 17';
 select count(*)
 from (select count(*)
 from involved i
@@ -194,9 +209,8 @@ group by i.movieid
   having count(distinct i.role) = (select count(*) from role)
 ) tmp;
 
-
 -- 18
-
+\echo 'Ex. 18';
 select count(*)
 from (
 select count(*)
@@ -204,8 +218,7 @@ from person p
   join involved i on p.id = i.personid
   join movie_genre mg on i.movieid = mg.movieid
   join genre g on mg.genre = g.genre
-  join (select g.genre from genre g where g.category = 'Lame') genres on genres.genre = g.genre
+where g.genre in (select genre from genre where category = 'Newsworthy')
 group by p.id
-  having count(distinct g.genre) = (select count(*) from genre where category = 'Lame')
+  having count(distinct g.genre) = (select count(*) from genre where category = 'Newsworthy')
 ) tmp;
-*/
