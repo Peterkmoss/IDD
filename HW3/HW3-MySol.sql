618587b6e7a6dcc53599255168ff1b0d7e2ad2bf
Peter Moss <peter@moss.dk>
2019-11-03 19:10:16 +0100

    Done with HW3

--- HW3/HW3-MySol.sql
+++ HW3/HW3-MySol.sql
@@ -11,7 +11,42 @@
 --! H1(hid, hs, hz)
 --! H2(hz, hc)
 
-drop table if exists RentalsNew;
+drop table if exists People cascade;
+drop table if exists H2 cascade;
+drop table if exists H1 cascade;
+drop table if exists RentalsNew cascade;
+
+create table People (
+    pid int primary key,
+    pn varchar(50) not null
+);
+
+insert into People(pid, pn)
+select pid, pn
+from Rentals
+group by pid, pn;
+
+create table H2 (
+    hz int primary key,
+    hc varchar(50) not null
+);
+
+insert into H2(hz, hc)
+select hz, hc
+from Rentals
+group by hz, hc;
+
+create table H1 (
+    hid int primary key,
+    hs varchar(50) not null,
+    hz int references H2(hz)
+);
+
+insert into H1(hid, hs, hz)
+select hid, hs, hz
+from Rentals
+group by hid, hs, hz;
+
 create table RentalsNew (
     pid int references People(pid),
     hid int references H1(hid),
@@ -19,24 +54,10 @@ create table RentalsNew (
     primary key (pid, hid)
 );
 
-drop table if exists People;
-create table People (
-    pid int primary key,
-    pn varchar(50) not null
-);
-
-drop table if exists H1;
-create table H1 (
-    hid int primary key,
-    hs varchar(50) not null,
-    hz int references H2(hz)
-);
-
-drop table if exists H2;
-create table H2 (
-    hz int primary key,
-    hc varchar(50) not null
-);
+insert into RentalsNew(pid, hid, s)
+select pid, hid, s
+from Rentals
+group by pid, hid, s;
 
 --! The normal form for the new schema is BCNF
 
@@ -48,20 +69,37 @@ create table H2 (
 --! Boats(bl, bno, z, bn, ssn)
 --! Zipcodes(z, t)
 
-drop table if exists BoatsNew;
-create table BoatsNew (
-    bl char(2),
-    bno int,
-    z int references Zipcodes(z),
-    bn varchar(50),
-    ssn char(10)
-    primary key (bl, bno)
-);
+drop table if exists Zipcodes cascade;
+drop table if exists BoatsNew cascade;
 
-drop table if exists Zipcodes;
 create table Zipcodes(
     z int primary key,
     t varchar(50)
 );
 
+insert into Zipcodes(z, t)
+select z, t
+from Boats
+group by z, t;
+
+create table BoatsNew (
+    bl char(2),
+    bno int,
+    z int references Zipcodes(z),
+    bn varchar(50),
+    ssn char(10),
+    primary key (bl, bno)
+);
+
+insert into BoatsNew(bl, bno, z, bn, ssn)
+select bl, bno, z, bn, ssn
+from Boats;
+
 --! The normal form for the new schema is 3NF
+
+--* Testing to see if all the entries are in the new relations
+select count(*) from Rentals;
+select count(*) from RentalsNew;
+
+select count(*) from Boats;
+select count(*) from BoatsNew;
